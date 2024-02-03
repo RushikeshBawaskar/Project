58b4659ff1ea855dffb60c27236d13ccd558b22e
RushikeshBawaskar <rushibawaskar2001@gmail.com>
2024-02-03 10:10:13 +0530

    rushi

---
+++ src/main/java/dev/Rushikesh/Movies/MovieRepository.java
@@ -0,0 +1,14 @@
+package dev.Rushikesh.Movies;
+
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId>{
+	
+	public Optional<Movie> findMovieByImdbId(String imdbId);
+	
+
+}
